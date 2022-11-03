6599fcc87a13f2ec31f6cad998aabb75b1e723c9
Tai Bo <tbo@occourts.org>
2022-11-02 22:05:49 -0700

    Adding method in ExpenseController to estimate expense of a given property, given the property address

--- reit-zillow-api/Web.Tests/ExpenseControllerTests.cs
+++ reit-zillow-api/Web.Tests/ExpenseControllerTests.cs
@@ -1,10 +1,16 @@
-﻿using Core.Dto;
+﻿using Core.Constants;
+using Core.Dto;
 using Core.Expense;
+using Core.Listing;
+using Core.Zillow;
+using Infrastructure.Expense;
+using Infrastructure.Listing;
 using Moq;
 using reit_zillow_api.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,24 +18,52 @@ namespace Web.Tests
 {
     public class ExpenseControllerTests
     {
-        private ExpenseController _expenseController;
-        private Mock<IExpenseEstimator> _mockExpenseEstimator;
+        private readonly ExpenseController _expenseController;
+        private readonly IExpenseEstimator _expenseEstimator;
+        private readonly Mock<IZillowClient> _mockZillowClient;
+        private readonly Mock<IListingParser> _mockListingParser;
 
         public ExpenseControllerTests()
         {
-            _mockExpenseEstimator = new Mock<IExpenseEstimator>();
-
-            _expenseController = new ExpenseController(_mockExpenseEstimator.Object);
+            // need to update to real expense estimator 
+            _expenseEstimator = new ExpenseEstimator(new MortgageExpenseEstimator(),
+                new PropertyTaxExpenseEstimator(),
+                new HomeOwnerInsuranceExpenseEstimator(),
+                new CapExExpenseEstimator(),
+                new RepairExpenseEstimator(),
+                new PropertyManagementExpenseEstimator(),
+                new MiscExpenseEstimator());
+            _mockZillowClient = new Mock<IZillowClient>();
+            _mockListingParser = new Mock<IListingParser>();
+            _expenseController = new ExpenseController(_expenseEstimator, _mockZillowClient.Object, _mockListingParser.Object);
         }
 
         [Fact]
         public void EstimateExpenses()
         {
             var estimateExpenseRequest = new EstimateExpensesRequest();
-            var expectedExpenseDetail = new ExpenseDetail();
-            _mockExpenseEstimator.Setup(estimator => estimator.EstimateExpenses(estimateExpenseRequest)).Returns(expectedExpenseDetail);
             var expenseDetail = _expenseController.EstimateExpenses(estimateExpenseRequest);
             Assert.NotNull(expenseDetail);
+        }
+
+        [Fact]
+        public void EstimateExpenses_GivenPropertyAddress()
+        {
+            // arrange 
+            var testAddress = "1234 Heaven St, Anaheim, CA";
+            var testHTML = "<html></html>";
+            _mockZillowClient.Setup(zillowClient => zillowClient.GetHtml(It.IsAny<string>())).ReturnsAsync(testHTML);
+            var testListingDetail = new ListingDetail()
+            {
+                ListingPrice = 600000,
+                YearBuilt = 1950,
+            };
+            _mockListingParser.Setup(listingParser => listingParser.Parse(It.IsAny<string>())).Returns(testListingDetail);
+            // act 
+            var expenseDetail = _expenseController.EstimateExpenses(testAddress).Result;
+            Assert.NotNull(expenseDetail);
+            Assert.True(expenseDetail.Total > 0);
+
         }
 
         [Fact]
--- reit-zillow-api/Web/Controllers/ExpenseController.cs
+++ reit-zillow-api/Web/Controllers/ExpenseController.cs
@@ -1,5 +1,8 @@
-﻿using Core.Dto;
+﻿using Core.Constants;
+using Core.Dto;
 using Core.Expense;
+using Core.Listing;
+using Core.Zillow;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
@@ -10,11 +13,15 @@ namespace reit_zillow_api.Controllers
     [ApiController]
     public class ExpenseController : ControllerBase
     {
-        private IExpenseEstimator _expenseEstimator;
-
-        public ExpenseController(IExpenseEstimator expenseEstimator)
+        private readonly IExpenseEstimator _expenseEstimator;
+        private readonly IListingParser _listingParser;
+        private readonly IZillowClient _zillowClient;
+        public ExpenseController(IExpenseEstimator expenseEstimator, IZillowClient zillowClient,
+            IListingParser listingParser)
         {
             _expenseEstimator = expenseEstimator;
+            _listingParser = listingParser;
+            _zillowClient = zillowClient;
         }
 
         [HttpGet]
@@ -25,6 +32,22 @@ namespace reit_zillow_api.Controllers
                 throw new ArgumentNullException(nameof(request));
             }
             return _expenseEstimator.EstimateExpenses(request);
+        }
+
+        [HttpGet]
+        public async Task<ExpenseDetail> EstimateExpenses(string propertyAddress)
+        {
+            var htmlListingDetail = await _zillowClient.GetHtml(propertyAddress);
+            var listingDetail = _listingParser.Parse(htmlListingDetail);
+            var estimateExpenseRequest = new EstimateExpensesRequest()
+            {
+                PropertyValue = listingDetail.ListingPrice,
+                InterestRate = 6.5,
+                PropertyAge = listingDetail.PropertyAge,
+                LoanProgram = LoanProgram.ThirtyYearFixed.ToString(),
+                RentAmount = 3000
+            };
+            return _expenseEstimator.EstimateExpenses(estimateExpenseRequest);
         }
     }
 }
