cb0f593dab9b86917bc6a92c380b9b6ce284070d
Tai Bo <tbo@occourts.org>
2022-12-05 22:37:40 -0800

    Added selling cost estimator

---
+++ reit-zillow-api/Core/SellingCostOrProfit/ISellingCostEstimator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.SellingCostOrProfit
+{
+    public interface ISellingCostEstimator
+    {
+        public double EstimateSellingCost(double purchasedValue, double currentValue);
+    }
+}
