549ae84c19390f86ce5ee76a19acd9ea5905e37a
Tai Bo <tbo@occourts.org>
2023-04-29 08:34:24 -0700

    Added navigation, search bar. WIP

--- reit-portal/src/app/app.module.ts
+++ reit-portal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SearchComponent } from './search/search.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SearchComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
